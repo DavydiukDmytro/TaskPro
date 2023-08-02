aefb9bd256a1ce913a373efcbc2eb59e319ce811
M.Mostova <mmainfo15@gmail.com>
2023-08-02 17:26:17 +0300

    fixed Welcome

--- src/components/StartContainer/StartContainer.jsx
+++ src/components/StartContainer/StartContainer.jsx
@@ -3,7 +3,7 @@ import css from './StartContainer.module.css';
 export const StartContainer = ({ children }) => {
   return (
     <div className={css.container}>
-      <div className={css.wrapp}>{children}</div>
+    {children}
     </div>
   );
 };
