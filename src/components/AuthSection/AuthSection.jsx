4884f219b96e7814ec092252d6212ef99d760d0e
DavydiukDmytro <davudyuck0501@gmail.com>
2023-08-03 21:38:26 +0300

    fix authsection

---
+++ src/components/AuthSection/AuthSection.jsx
@@ -0,0 +1,13 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+export const AuthSection = () => {
+  return (
+    <div>
+      <div>
+        <NavLink>Registration</NavLink>
+        <NavLink>Log In</NavLink>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
