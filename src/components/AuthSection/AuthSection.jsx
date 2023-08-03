7b37dfefd652f0e8c71076785c259767c48d0b4d
M.Mostova <mmainfo15@gmail.com>
2023-08-03 19:13:14 +0300

    add styles for section

--- src/components/AuthSection/AuthSection.jsx
+++ src/components/AuthSection/AuthSection.jsx
@@ -1,11 +1,26 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import css from './AuthSection.module.css';
 
 export const AuthSection = () => {
   return (
-    <div>
-      <div>
-        <NavLink >Registration</NavLink>
-        <NavLink>Log In</NavLink>
+    <div className={css.container}>
+      <div className={css.link__container}>
+        <NavLink
+          className={({ isActive }) => {
+            return isActive ? css.active : css.link;
+          }}
+          to="register"
+        >
+          Registration
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => {
+            return isActive ? css.active : css.link;
+          }}
+          to="login"
+        >
+          Log In
+        </NavLink>
       </div>
       <Outlet />
     </div>
