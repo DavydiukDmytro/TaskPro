76fb410e1fa54a6aea75eb72dbe67cdcd81b1b57
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-08-03 19:18:53 +0300

    add var css

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,17 +1,9 @@
-import { ThemeSwitcher } from 'components/ThemeSwitcher/ThemeSwitcher';
-import { useThemeContext } from '../../components/ThemeSwitcher/ThemeContext';
 import css from './Home.module.css';
 
 const Home = () => {
-  // const { currentTheme } = useThemeContext();
-
-  // const appStyles = {
-  //   backgroundColor: currentTheme.palette.background.primary,
-  // };
   return (
     <div className={css.homePage}>
       <h1>Home</h1>
-      <ThemeSwitcher></ThemeSwitcher>
     </div>
   );
 };
