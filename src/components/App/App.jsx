55e1e2f764168c9ca688866e82bd0e007aff0725
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-08-04 19:58:56 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,20 @@
-import { Auth } from 'page/Auth';
-import { Home } from 'page/Home';
-import { Welcome } from 'page/Welcome';
+// import { Auth } from 'page/Auth';
+// import { Home } from 'page/Home';
+// import { Welcome } from 'page/Welcome';
 
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from '../ThemeSwitcher/ThemeContext';
+import { EditProfileForm } from 'components/EditProfileForm/EditProfileForm';
 
 export const App = () => {
   return (
     <ThemeProvider>
-      <Routes>
+      <EditProfileForm />
+      {/* <Routes>
         <Route path="/welcome" element={<Welcome />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/" element={<Home />} />
-      </Routes>
+      </Routes> */}
     </ThemeProvider>
   );
 };
--- src/components/EditProfileForm/EditProfileForm.jsx
+++ src/components/EditProfileForm/EditProfileForm.jsx
@@ -13,12 +13,23 @@ export const EditProfileForm = user => {
     email: user.email || '',
     password: user.password || '',
   };
+  const EMAIL_REGX = `^(([^<>()\[\]\\.,;:\s@"]+(.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/`;
+  const PASSWORD_REGEX =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,64}$/;
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(32, 'Too Long!')
+      .required('Name is required'),
     phone: Yup.string().required('Phone is required'),
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    password: Yup.string().required('Password is required'),
+    email: Yup.string()
+      .matches(EMAIL_REGX, 'Invalid email address')
+      .email('Invalid email')
+      .required('Email is required'),
+    password: Yup.string()
+      .matches(PASSWORD_REGEX, 'Please enter a strong password')
+      .required('Password is required'),
   });
 
   const handleSubmit = async values => {
