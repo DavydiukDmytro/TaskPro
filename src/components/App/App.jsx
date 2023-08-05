e17c156c1b0dcda2ad6a69d7baada4416741511d
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-08-05 06:54:52 +0300

    add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,33 @@
-import { Auth } from 'page/Auth';
-import { Home } from 'page/Home';
-import { Welcome } from 'page/Welcome';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
-import { Route, Routes } from 'react-router-dom';
+import { Welcome } from '../../page/Welcome';
+import { Auth } from '../../page/Auth';
+import { Home } from '../../page/Home';
+import { ScreensPage } from '../../page/ScreensPage';
+import { RegistrationForm } from '../RegisterForm';
+import { LoginForm } from '../LoginForm';
+
 import { ThemeProvider } from '../ThemeSwitcher/ThemeContext';
 
 export const App = () => {
+  const isAuthenticated = true;
+
   return (
     <ThemeProvider>
       <Routes>
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/auth" element={<Auth />}>
+          <Route path="register" element={<RegistrationForm />} />
+          <Route path="login" element={<LoginForm />} />
+        </Route>
+        <Route
+          path="/home"
+          element={isAuthenticated ? <Home /> : <Navigate to="/auth/login" />}
+        />
+        <Route path="/home" element={<Home />}>
+          <Route path=":boardName" element={<ScreensPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </ThemeProvider>
   );
