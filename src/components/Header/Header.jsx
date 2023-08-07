503827dda1d05bb496862a89ae6a56b181d43726
M.Mostova <mmainfo15@gmail.com>
2023-08-08 01:22:53 +0300

    fixed Sidebr

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
-import { ThemeSwitcher } from 'components/ThemeSwitcher/ThemeSwitcher';
 import css from './Header.module.css';
+import { ThemeSwitcher } from 'components/ThemeSwitcher/ThemeSwitcher';
 import { UserInfo } from 'components/UserInfo';
-
 
 export const Header = () => {
   return (
     <header className={css.header}>
-
-
       <ThemeSwitcher />
       <UserInfo />
     </header>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,22 @@
+import { useState } from 'react';
+import svgSprite from '../../assets/svg/symbol-defs.svg';
+import css from './Sidebar.module.css';
 import { BoardsList } from 'components/BoardsList';
 import { ButtonLogout } from 'components/ButtonLogout';
-import css from './Sidebar.module.css';
-// import svgSprite from '../../assets/svg/symbol-defs.svg';
 import { BoardTitle } from 'components/BoardTitle/BoardTitle';
 import { NeedHelp } from 'components/NeedHelp/NeedHelp';
 import { Logo } from 'components/Logo/Logo';
-import svgSprite from '../../assets/svg/symbol-defs.svg';
-import { useState } from 'react';
 
 export const Sidebar = () => {
   const [isSidebarVisible, setSidebarVisible] = useState(false);
 
   const toggleSidebar = e => {
     const data = e.target.getAttribute('data-action');
-    console.log(data);
     if (data === 'open' || data === 'close') {
       setSidebarVisible(!isSidebarVisible);
     }
-    // console.log(e.target.getAttribute('data-action'));
-    return;
   };
+
   return (
     <>
       {!isSidebarVisible && (
