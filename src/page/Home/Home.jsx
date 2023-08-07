65d295f7b838d8ddfc6b5b7bb5cc11f5b4c937ce
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-08-07 21:26:12 +0300

    done

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,10 +1,8 @@
 import { Sidebar } from 'components/Sidebar/Sidebar';
 import { ThemeSwitcher } from 'components/ThemeSwitcher/ThemeSwitcher';
-// import { UserInfo } from 'components/UserInfo';
 import { UserInfo } from '../../components/UserInfo';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import BlankPage from '../BlankPage/BlankPage';
 
 export const Home = () => {
   return (
@@ -12,7 +10,6 @@ export const Home = () => {
       <header>
         <button>open sidebar</button>
         <ThemeSwitcher />
-        <BlankPage />
         <UserInfo />
       </header>
       <main>
