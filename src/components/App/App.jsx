c8d7c59cc394550e6c2d2888d12f5c1a937bb29c
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-08-08 23:02:26 +0300

    add publicrout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,37 +5,30 @@ import { PrivateRoute } from 'components/PrivateRoute/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 import { SharedLayoutStart } from '../SharedLayoutStart';
 import { Home } from 'page/Home';
+import { PublicRoute } from 'components/PublicRoute/PublicRoute';
 
-// import { Welcome } from '../../page/Welcome';
-// import { Auth } from '../../page/Auth';
-// import { Home } from '../../page/Home';
-// import { ScreensPage } from '../../page/ScreensPage';
-
-// import { LoginForm } from '../LoginForm';
-
-// const SharedLayoutStart = lazy(() => import('../SharedLayoutStart'));
 const StartPage = lazy(() => import('../../page/Start/Start'));
 const AuthPage = lazy(() => import('../../page/Auth/Auth'));
 const RegistrationForm = lazy(() => import('../RegisterForm/RegistrationForm'));
 const LoginForm = lazy(() => import('../LoginForm/LoginForm'));
 const ScreensPage = lazy(() => import('../../page/ScreensPage/ScreensPage'));
 
-
 export const App = () => {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchUser());
-  // }, [dispatch]);
-
   return (
     <ThemeProvider>
       <Routes>
         <Route path="/" element={<SharedLayoutStart />}>
           <Route index element={<StartPage />} />
-          <Route path="/auth" element={<AuthPage />}>
+          <Route
+            path="/auth"
+            element={
+              <PublicRoute>
+                <AuthPage />
+              </PublicRoute>
+            }
+          >
             <Route path="register" element={<RegistrationForm />} />
             <Route path="login" element={<LoginForm />} />
           </Route>
