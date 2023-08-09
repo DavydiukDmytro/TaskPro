90ff7c4ad6cd80750afb8a22e1af22deecfad440
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-08-09 22:42:55 +0300

    create data picker

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,23 @@
+// import * as React from 'react';
+// import dayjs, { Dayjs } from 'dayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+// import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+
+// export const Calendar = () => {
+//   const [value, setValue] =
+//     (React.useState < Dayjs) | (null > dayjs('2022-04-17'));
+
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//       <DemoContainer components={['DatePicker']}>
+//         <DatePicker
+//           label="Date"
+//           value={value}
+//           onChange={newValue => setValue(newValue)}
+//         />
+//       </DemoContainer>
+//     </LocalizationProvider>
+//   );
+// };
