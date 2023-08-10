c0a4aa9768ebaa8764a16a59f0bc5692487d9ba2
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2023-08-10 21:14:27 +0300

    AddCard

---
+++ src/components/Add Card/Add Card.jsx
@@ -0,0 +1,157 @@
+import { Formik, Field, Form } from 'formik';
+import { useDispatch } from 'react-redux';
+// import {AddCard  } from '...';
+// import {CanedarValue } from '...';
+import { object, string } from 'yup';
+import svgSprite from '../../assets/svg/symbol-defs.svg';
+import css from './AddCard.module.css';
+
+const initialValuesForm = {
+  title: '',
+  textarea: '',
+  color: '',
+  date: '',
+};
+console.log(css);
+const schema = object({
+  title: string().trim().required('Required'),
+  textarea: string().trim().required('Required'),
+  color: string().trim().required('Required'),
+  date: string().trim().required('Required'),
+});
+
+export const AddCard = () => {
+  const handleSubmit = async (values, { resetForm }) => {
+    console.log(values);
+    // await dispatch(AddCard(values));
+    resetForm();
+  };
+
+  return (
+    <div className={css.wrapper}>
+      <button className={css.buttonClose}>
+        <svg width={18} height={18}>
+          <use className={css.svg} href={svgSprite + '#icon-x-close'} />
+        </svg>
+      </button>
+      <Formik
+        initialValues={initialValuesForm}
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+        validateOnBlur
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isValid,
+          dirty,
+        }) => (
+          <Form className={css.form}>
+            <div className={css.form__field}>
+              <p className={css.text}>Add Card</p>
+              <input
+                className={css.form__input}
+                autoComplete="title"
+                placeholder="Tittle"
+                type="text"
+                name="title"
+                onChange={handleChange}
+                value={values.title}
+              />
+              {touched.title && errors.title && (
+                <p className={css.form__error}>{errors.title}</p>
+              )}
+            </div>
+            <div className={css.form__field}>
+              <textarea
+                className={css.form__textarea}
+                type="text"
+                rows="6"
+                placeholder="Description"
+                name="textarea"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.textarea}
+              />
+              {touched.textarea && errors.textarea && (
+                <p className={css.form__error}>{errors.textarea}</p>
+              )}
+            </div>
+            <div className={css.wrap}>
+              <p className={css.textLabel} id="my-radio-group">
+                Label color
+              </p>
+              <div role="group" aria-labelledby="my-radio-group">
+                <label className={css.label}>
+                  <Field
+                    type="radio"
+                    name="color"
+                    className={css.real__radio}
+                    value="#8fa1d0"
+                  />
+                  <span className={css.сustom__radio}></span>
+                </label>
+
+                <label className={css.label}>
+                  <Field
+                    type="radio"
+                    name="color"
+                    className={css.real__radio}
+                    value="#e09cb5"
+                  />
+                  <span className={`${css.сustom__radio} ${css.color2}`}></span>
+                </label>
+                <label className={css.label}>
+                  <Field
+                    type="radio"
+                    name="color"
+                    className={css.real__radio}
+                    value="#bedbb0"
+                  />
+                  <span className={`${css.сustom__radio} ${css.color3}`}></span>
+                </label>
+                <label className={css.label}>
+                  <Field
+                    type="radio"
+                    name="color"
+                    className={css.real__radio}
+                    value="#ffffff"
+                  />
+                  <span className={`${css.сustom__radio} ${css.color4}`}></span>
+                </label>
+              </div>
+            </div>
+            <div className={css.wrap}>
+              <label className={css.textLabel}>Deadline</label>
+              <Field
+                component="select"
+                className={css.select}
+                id="date"
+                name="date"
+              >
+                <option className={css.option} value="CanedarValue">
+                  {'CanedarValue'}
+                </option>
+                <option className={css.option} value="CanedarValue1">
+                  {'CanedarValue'}
+                </option>
+              </Field>
+            </div>
+            <div className={css.wrapButton}>
+              <button type="submit" className={css.button}>
+                <svg width="28px" height="28px" className={css.icon}>
+                  <use href={svgSprite + '#icon-plus'} />
+                </svg>
+                Add
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
