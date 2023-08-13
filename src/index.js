00da2a66ee9d7e69fcf88628ff0118923346eb55
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-08-13 17:41:19 +0300

    add persist

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { App } from 'components/App';
 import './utils/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from 'store/store';
+import store, { persistor } from 'store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Loader } from 'components/Loader';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="/TaskPro">
-        <App />
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
