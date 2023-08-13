d4e4772c3ed6a89a73b53db443b4ff3f99de5b53
DavydiukDmytro <davudyuck0501@gmail.com>
2023-08-13 11:45:38 +0300

    fix modal edit column

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import css from './Header.module.css';
 import { ThemeSwitcher } from 'components/ThemeSwitcher/ThemeSwitcher';
 import { UserInfo } from 'components/UserInfo';
-import { EditColumn } from 'components/EditColumn';
 
 export const Header = () => {
   return (
     <header className={css.header}>
-      <EditColumn />
       <ThemeSwitcher />
       <UserInfo />
     </header>
