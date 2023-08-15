52e6cdbea31a799183db8af65583e45ab7890049
DavydiukDmytro <davudyuck0501@gmail.com>
2023-08-15 20:45:17 +0300

    delete

--- src/components/CardTitle/CardTitle.jsx
+++ src/components/CardTitle/CardTitle.jsx
@@ -5,9 +5,11 @@ import { deleteColumn } from 'store/boards/operationsBoards';
 import { useState } from 'react';
 import { Modal } from 'components/Modal';
 import { EditColumn } from 'components/EditColumn';
+import { Delete } from 'components/Delete';
 
 export const CardTitle = ({ title, id }) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
   const dispatch = useDispatch();
 
   const handleEditColumn = () => {
@@ -31,7 +33,7 @@ export const CardTitle = ({ title, id }) => {
             </button>
           </li>
           <li>
-            <button className={css.button} onClick={() => handleDeleteColumn()}>
+            <button className={css.button} onClick={() => setIsDelete(true)}>
               <svg width={16} height={16} stroke="var(--primary-text-color)">
                 <use href={svgSprite + '#icon-trash-04'} />
               </svg>
@@ -39,6 +41,15 @@ export const CardTitle = ({ title, id }) => {
           </li>
         </ul>
       </div>
+      {isDelete && (
+        <Modal>
+          <Delete
+            handleClose={() => setIsDelete(false)}
+            handleDelete={() => handleDeleteColumn()}
+            text={'You really want to remove the column with all the cards?'}
+          />
+        </Modal>
+      )}
       {isOpenModal && (
         <Modal
           handleClose={() => {
